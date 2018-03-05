ab1fc4428a28e2a364fed4616a818befd66c6168
nelsondominguez <nelsonjr73090@gmail.com>
2018-03-04 19:57:29 -0500

    added menus and fixed menu issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,53 +1,36 @@
+window.onload = init();
 
+function init() {
 
+     window.addEventListener('scroll', function (e) {
+          var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+               shrinkOn = 300,
+               header = document.querySelector("header");
+          if (distanceY > shrinkOn) {
+               classie.add(header, "smaller");
+          } else {
+               if (classie.has(header, "smaller")) {
+                    classie.remove(header, "smaller");
+               }
+          }
+     });
 
+     $.ajax({
+          method: 'GET',
+          url: 'assets/data/menu.json',
+          dataType: 'json',
+          success: function (data) {
 
+               var menu = menuBuilder(data.menu);
+               $('#primary_nav_wrap').html(menu);
+               
+          },
+          error: function () {
+               console.log('bad');
+          }
+     });
 
-
-
-
-function init() {			
-			window.addEventListener('scroll', function(e){
-				var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-					shrinkOn = 300,
-						header = document.querySelector("header");				
-							if (distanceY > shrinkOn) {
-							classie.add(header,"smaller");					
-							} else {
-							if (classie.has(header,"smaller")) {
-						classie.remove(header,"smaller");
-					}
-				}
-			});
-	
-			$.ajax({
-				method: 'GET',
-				url: 'assets/data/menu.json',
-				dataType: 'json',
-				success: function(data){
-					
-					
-					var menu = menuBuilder(data.menu);
-					$('#primary_nav_warp').html(menu);
-				
-			},
-			error: function(){
-				console.log('call was bad');
-			}
-				
-			});
-			
-	
-	
-	
-	
-	
-	
-	
-	
-		}
-		
-	   window.onload = init();
+}
 
 
 
@@ -56,15 +39,10 @@ function menuBuilder(obj) {
      var theMenu = '';
 
      if (obj.length > 0) {
-		 
 		theMenu = theMenu + '<ul>';
-		 
 		obj.forEach(function (item) {
-			
 			theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
-			
 			if (item.Menus.length > 0) {
-				
 				theMenu = theMenu + menuBuilder(item.Menus);
 			}
 			theMenu = theMenu + '</li>';
